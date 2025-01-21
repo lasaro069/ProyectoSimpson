c6f96043d60eccc2c5a671fd9ce10c4a5927486c
lasaro069 <lasaro069@gmail.com>
2025-01-21 02:33:11 -0500

    se traen lospersonajes meidante fetch

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Proyecto del primer semestre del Tecnico Laboral en Programación en Klazia
 
+imagen del header ""https://santifx.files.wordpress.com/2017/09/intro-nubes-simpsons-santifx-preview.png"
+
+API de los simpsons: "https://thesimpsonsquoteapi.glitch.me"
---
+++ js/index.js
@@ -0,0 +1,38 @@
+const grid = document.querySelector(".grid");
+
+const fetchCharacters = (quantity) => {
+
+    fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?count=${quantity}`)
+        .then((response) => response.json())
+        .then(data => {
+            console.log(data);
+            createCharacterCards(data);
+        })
+}
+
+fetchCharacters(10);
+
+// Creamos una funcion 'createCharacterCard' que recibe la informacion del fetch para pintarlo en el html
+const createCharacterCards = (data) => {
+    let cards = "";
+    data.forEach(item => {
+        cards += `
+            <div class="character_card">
+                <div class="character_car">
+                    <div class="img_container">
+                        <img class="character_img" src="${item.image}" alt="">
+                    </div>
+                    <div class="info_character">
+                        <h4 class="character_name">${item.character}</h4>
+                        <small>${item.character} Dice:</small>
+                        <p>${item.quote}</p>
+                    </div>
+                </div>
+            </div>
+            
+
+        `;
+    });
+
+    grid.innerHTML = cards;
+}
