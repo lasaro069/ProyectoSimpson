ec5af0530f505f87631c8afc27e2b01c6494f724
lasaro069 <lasaro069@gmail.com>
2025-01-22 22:19:07 -0500

    se consolidan los cambios para el proyecto final

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const grid = document.querySelector(".grid");
 
+// Función que llama a los personajes mediante el 'fetch'
 const fetchCharacters = (quantity) => {
 
     fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?count=${quantity}`)
@@ -13,39 +14,32 @@ const fetchCharacters = (quantity) => {
         })
 }
 
-fetchCharacters(23);
+fetchCharacters(52);    // asignamos la cantidad de elementos que queremos traer mediante el 'fetch'
 
-
-
-const search_btn = document.querySelector(".search_btn");
-
-
-
-
-
-
+// funcipon que llama al personaje introducido en el cuadro de texto
 const fetchCharacterByName = () => {
-    const search_input = document.querySelector(".search_input");
-    fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?character=${search_input.value}`)
-        .then((response) => response.json())
-        .then(data =>{
-            createCharacterCards(data);
-            }
-        )
 
-
-
+    if (search_input.value == "") { // 'search_input.value' evalua si el campo de texto
+        recargarPagina();   // si es 'true' recargará la página
+    }else { // si no, realizará la búsqueda del elemento 
+        const search_input = document.querySelector(".search_input");
+        fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?character=${search_input.value}`)
+            .then((response) => response.json())
+            .then(data =>{
+                createCharacterCards(data);
+            });
+    }
 }
 
-
-
-
-
+const search_input = document.querySelector(".search_input");   // variable que escucha el cuadro de texto con class="search_input"
 
 //creamos el evento de escucha
-search_btn.addEventListener("click", fetchCharacterByName);
+search_input.addEventListener("change", fetchCharacterByName);  // 'change' realiza la búsqueda del cuadro de texto al dar 'enter'
 
-
+// función que recarga la página 
+function recargarPagina() {
+    location.reload();
+}
 
 // Creamos una funcion 'createCharacterCard' que recibe la informacion del fetch para pintarlo en el html
 const createCharacterCards = (data) => {
@@ -67,7 +61,7 @@ const createCharacterCards = (data) => {
             
 
         `;
-    });
+    })
 
     grid.innerHTML = cards;
 }
